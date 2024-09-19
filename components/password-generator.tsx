c156a72018e0a239ba3b73229f8549cdf6d2d864
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-09-19 12:00:22 +0500

    Update password-generator.tsx

--- components/password-generator.tsx
+++ components/password-generator.tsx
@@ -86,7 +86,7 @@ export default function GeneratePassword() {
             backgroundPosition: 'center'
         }}
         >
-            <Card className='border-gray-900 border-2 w-full max-w-md p-6 rounded-3xl shadow-2xl bg-gradient-to-r from-green-500 via-green-300 to-green-500'>
+            <Card className='border-gray-900 border-2 w-full max-w-md p-6 rounded-3xl shadow-2xl bg-gradient-to-r from-green-400 via-green-300 to-green-400'>
                 <div className='mx-auto max-w-md space-y-6'>
                     <div className='space-y-2 text-center'>
                         <h1 className='font-bold text-3xl'>Password Generator</h1>
